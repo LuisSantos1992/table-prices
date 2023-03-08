d88fb32851efe12af270f7f53cfc849b664478bd
renemetalero <60973796+renemetalero@users.noreply.github.com>
2023-03-08 16:41:21 -0600

    Creacion de tablas
    Se crearon las tablas con sus relaciones

---
+++ comercio-electronico/src/main/java/com/comercio/electronico/model/Prices.java
@@ -0,0 +1,40 @@
+package com.comercio.electronico.model;
+
+import java.util.Date;
+
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name="prices")
+public class Prices {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idPrice;
+	
+	private String name;
+	private Date startDate;
+	private Date endDate;
+	private int priceList;
+	private int priority;
+	private Double price;
+	private String curr;
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "idProduct")
+	private Products product;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "idBrand")
+	private Brands brand;
+
+}
