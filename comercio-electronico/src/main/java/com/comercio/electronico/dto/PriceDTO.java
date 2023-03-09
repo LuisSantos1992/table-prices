0a13fe985b79e668b923c11ece12481561959c74
renemetalero <60973796+renemetalero@users.noreply.github.com>
2023-03-09 17:53:58 -0600

    Formato de fechas
    se agrego formato de fechas

--- comercio-electronico/src/main/java/com/comercio/electronico/dto/PriceDTO.java
+++ comercio-electronico/src/main/java/com/comercio/electronico/dto/PriceDTO.java
@@ -2,10 +2,12 @@ package com.comercio.electronico.dto;
 
 import com.comercio.electronico.model.Brands;
 import com.comercio.electronico.model.Products;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,8 +16,11 @@ public class PriceDTO {
 
     private Integer priceList;
     private String name;
-    private Date startDate;
-    private Date endDate;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime startDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime endDate;
     private int priority;
     private Double price;
     private String curr;
