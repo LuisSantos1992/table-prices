1e969ca18b2944b787b3931dca872dafae35dc3d
Tito Merino <33304055+titomerino@users.noreply.github.com>
2023-03-09 09:24:58 -0600

    Se agregaron DTO
    DTO para promociones

--- comercio-electronico/src/main/java/com/comercio/electronico/controller/ConsultaPriceController.java
+++ comercio-electronico/src/main/java/com/comercio/electronico/controller/ConsultaPriceController.java
@@ -3,9 +3,14 @@ package com.comercio.electronico.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.comercio.electronico.dto.PromotionDTO;
+import com.comercio.electronico.dto.PromotionResponseDTO;
 
 @RestController
 @RequestMapping("api/promotion")
@@ -16,4 +21,11 @@ public class ConsultaPriceController {
     private String getPrice() {
         return "Version 1.0";
     }
+    
+    @PostMapping(value="/getPromotion", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    private PromotionResponseDTO responsePromotion(@RequestBody PromotionDTO promotion) {
+    	PromotionResponseDTO promotionObj = new PromotionResponseDTO();
+    	return promotionObj;
+	}
 }
