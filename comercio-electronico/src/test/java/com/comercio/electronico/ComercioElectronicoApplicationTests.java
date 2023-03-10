87fbc8bdbc4876d1a69d273b03c7e80567d64eb8
LuisSantos1992 <bigcole1992@gmail.com>
2023-03-10 10:54:18 -0600

    Update ComercioElectronicoApplicationTests.java

--- comercio-electronico/src/test/java/com/comercio/electronico/ComercioElectronicoApplicationTests.java
+++ comercio-electronico/src/test/java/com/comercio/electronico/ComercioElectronicoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.comercio.electronico;
 
+import com.comercio.electronico.controller.ConsultaPriceController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,6 +10,8 @@ class ComercioElectronicoApplicationTests {
 	@Test
 	void contextLoads() {
 		System.out.println("");
+		ConsultaPriceController ob = new ConsultaPriceController();
+		ob.
 	}
 
 }
