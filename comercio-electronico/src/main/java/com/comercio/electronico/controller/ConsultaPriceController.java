1bbe11ae2b56c5659f658f17dd93d57126590e20
LuisSantos1992 <bigcole1992@gmail.com>
2023-03-10 10:51:53 -0600

    Update Params
    Update Params

--- comercio-electronico/src/main/java/com/comercio/electronico/controller/ConsultaPriceController.java
+++ comercio-electronico/src/main/java/com/comercio/electronico/controller/ConsultaPriceController.java
@@ -18,7 +18,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+//CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("api/promotion")
 public class ConsultaPriceController {
