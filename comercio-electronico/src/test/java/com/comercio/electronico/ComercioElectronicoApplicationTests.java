2b8f99dd5be7394a31d117ab7ad9f8ca54d468ee
LuisSantos1992 <bigcole1992@gmail.com>
2023-03-10 10:55:40 -0600

    Correcion test

--- comercio-electronico/src/test/java/com/comercio/electronico/ComercioElectronicoApplicationTests.java
+++ comercio-electronico/src/test/java/com/comercio/electronico/ComercioElectronicoApplicationTests.java
@@ -1,6 +1,5 @@
 package com.comercio.electronico;
 
-import com.comercio.electronico.controller.ConsultaPriceController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,8 +9,6 @@ class ComercioElectronicoApplicationTests {
 	@Test
 	void contextLoads() {
 		System.out.println("");
-		ConsultaPriceController ob = new ConsultaPriceController();
-		ob.
 	}
 
 }
