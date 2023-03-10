7e9c63d17adb25e0b1a5f2e6dec7ce157ab49680
LuisSantos1992 <bigcole1992@gmail.com>
2023-03-09 18:36:35 -0600

    Add Correcio data.sql

--- comercio-electronico/src/main/java/com/comercio/electronico/model/Prices.java
+++ comercio-electronico/src/main/java/com/comercio/electronico/model/Prices.java
@@ -28,11 +28,11 @@ public class Prices {
 	
 	
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "idProduct")
+	@JoinColumn(name = "id")
 	private Products product;
 	
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "idBrand")
+	@JoinColumn(name = "brandId")
 	private Brands brand;
 
 }
